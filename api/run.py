838348f390eea58484321d9f9aff3a7029f33cee
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-04 22:04:41 +0200

    template register and enviroments

--- api/run.py
+++ api/run.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.models import User, StructTableDbUsers
 
 app = FastAPI()
 
+app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello New World"}
+templates = Jinja2Templates(directory="app/templates")
+
+create_db = StructTableDbUsers()
+create_db.create_table()
+
+# REGISTER PAGE
+@app.get("/", response_class = HTMLResponse)
+async def register_index(request: Request):
+     return templates.TemplateResponse("index.html", {"request": request})
+
+
+# SEND USERS DATA IN DBB
+@app.post("/register")
+async def save_data_user(user: User):
+     print(user)
+
+
+if __name__ == "__main__":
+     register_index()
