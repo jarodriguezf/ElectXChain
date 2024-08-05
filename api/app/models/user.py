59d1e1c9010273131bfd8ac84d0a7ade2ca5994e
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-05 20:15:15 +0200

    model user

---
+++ api/app/models/user.py
@@ -0,0 +1,33 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel
+
+
+# USER CLASS AND STRUCTURE
+class UserSchema(BaseModel):
+    name: str
+    nie: str
+    birth: date
+    province: str
+    genre: Optional[str] = None
+    number_tel: int
+    pub_key: Optional[bytes] = None
+    priv_key: Optional[bytes] = None
+    activate: int = 0
+
+class User():
+    def __init__(self, name: str, nie: str, birth: date, province: str, genre: str, number_tel: int, pub_key: Optional[bytes], 
+                priv_key: Optional[bytes], activate: int = 0):
+        self.name = name
+        self.nie = nie
+        self.birth = birth
+        self.province = province
+        self.genre =  genre
+        self.number_tel = number_tel
+        self.pub_key = pub_key
+        self.priv_key = priv_key
+        self.activate = activate
+
+    def get_user(self):
+        return f"User(name={self.name}, nie={self.nie}, birth={self.birth}, province={self.province}, genre={self.genre}, number_tel={self.number_tel}, pub_key={self.pub_key}, priv_key={self.priv_key}, activate={self.activate})"
+    
