2ebe44aef9db162c551d3473c2d59fc5520a7563
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-05 21:48:31 +0200

    include bussiness logic validation users fields

--- api/run.py
+++ api/run.py
@@ -4,8 +4,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import ValidationError
+from app.register import nie_validation, is_adult
 from app.models.user import User,  UserSchema
-from app.models.db import StructTableDbUsers, InsertDataDbUsers
+from app.models.db import StructTableDbUsers, InsertDataDbUsers, ShowNieExists, ShowTelephoneExists
 
 app = FastAPI()
 
@@ -40,19 +41,40 @@ async def register_index(request: Request):
 @app.post("/register")
 async def save_data_user(user: UserSchema):
      try:
+          # CLASSES
           insertNewData = InsertDataDbUsers()
+          NIEexists = ShowNieExists()
+          TelExists = ShowTelephoneExists()
 
           user_instance = User(**user.dict())
           logger.debug(f"Received user instance: {user_instance.get_user()}")
 
+          # VALIDATION FIELDS
+          if not nie_validation(user_instance.nie):
+               logger.error('Error: NIE incorrect')
+               raise HTTPException(status_code=400, detail="Incorrect NIE format")
+          
+          if not is_adult(user_instance.birth):
+               logger.error('Error: you need to be 18 years or older')
+               raise HTTPException(status_code=400, detail="User must be 18 years or older")
+          
+          if NIEexists.show_nie_exists(user_instance.nie):
+               logger.error('Error: NIE already exists for a user')
+               raise HTTPException(status_code=400, detail="User exists for that NIE")
+          
+          if TelExists.show_telephone_exists(user_instance.number_tel):
+               logger.error('Error: Telephone already exists for a user')
+               raise HTTPException(status_code=400, detail="User exists for that Telephone")
+          # SAVE THE USER
           insertNewData.insert_data_db(user_instance=user_instance)
           return {"message": "User data received"}
+     
      except ValidationError as e:
         logger.error(f'ValidationError: {e}')
         raise HTTPException(status_code=422, detail="Validation error")
      except ValueError as e:
           logger.error(f'ValueError: {e}')
-          raise HTTPException(status_code=400, detail="Invalid user data")
+          raise HTTPException(status_code=400, detail=str(e))
      except Exception as e:
         logger.error(f'Unexpected error: {e}')
         raise HTTPException(status_code=500, detail="Internal Server Error")
