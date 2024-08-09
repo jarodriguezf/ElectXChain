e6e0a818bba12ec65bf76b8a5c9ec6d515562403
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-09 22:04:26 +0200

    function validation and get token

---
+++ api/app/autentication.py
@@ -0,0 +1,44 @@
+import time
+import pyotp
+import os
+import logging
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def manager_key():
+    key = os.getenv('MANAGER_KEY_OTP', 'default_key')
+    if key == 'default_key':
+        logger.error('Error: The manager key is not configured')
+        return
+    return key
+
+def topt_instance(key, digits=6, interval=120):
+    return pyotp.TOTP(key, digits=6, interval=120)
+    
+
+# RETURN THE 2FA TOKEN
+def get_token() -> str:
+    key = manager_key()
+    if not key:
+        raise Exception('Error: Cant generated the OTP code.')
+    
+    totp = topt_instance(key, digits=6, interval=120)
+    logger.debug(f'Code generated: {totp.now()}')
+    return totp.now()
+
+    
+# VALIDATE THE CODE 2FA
+def validate_token(input_token: str) -> bool:
+    key = manager_key()
+    if not key:
+        raise Exception('Error: Cant validated the OTP code.')
+    
+    totp = topt_instance(key, digits=6, interval=120)
+    if not totp.verify(input_token):
+        logger.error('Error: In the verification of the code') 
+        return False
+    logger.info('Code successfully verified')
+    return True
