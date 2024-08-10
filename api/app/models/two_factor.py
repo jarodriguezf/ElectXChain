dd37b3349ef15ca6df760878804a8569497282df
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-10 19:06:56 +0200

    new structure with optional values

--- api/app/models/two_factor.py
+++ api/app/models/two_factor.py
@@ -1,4 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class TokenSchema(BaseModel):
+    number_tel: Optional[int] = None
+    id: Optional[str] = None
+
+class TokenInputSchema(BaseModel):
     token: str
