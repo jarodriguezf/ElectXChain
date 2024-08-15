14cacab8807383638ea142989bcfc51887189e48
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-15 18:30:55 +0200

    new field vote

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -24,10 +24,11 @@ class UserSchema(BaseModel):
     pub_key: Optional[bytes] = None
     priv_key: Optional[bytes] = None
     activate: int = 0
+    voted: int = 0
 
 class User():
     def __init__(self, name: str, dni: str, birth: date, province: str, genre: str, number_tel: int, pub_key: Optional[bytes], 
-                priv_key: Optional[bytes], activate: int = 0):
+                priv_key: Optional[bytes], activate: int = 0, voted: int = 0):
         self.name = name
         self.dni = dni
         self.birth = birth
@@ -37,6 +38,7 @@ class User():
         self.pub_key = pub_key
         self.priv_key = priv_key
         self.activate = activate
+        self.voted = voted
 
     def get_user(self):
         return f"User(name={self.name}, dni={self.dni}, birth={self.birth}, province={self.province}, genre={self.genre}, number_tel={self.number_tel}, pub_key={self.pub_key}, priv_key={self.priv_key}, activate={self.activate})"
