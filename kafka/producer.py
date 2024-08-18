98fa193bcb5dce6595aa14158e2f0e40aafee44f
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-18 20:48:54 +0200

    create producer kafka -initital-

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,16 +1,23 @@
 from confluent_kafka import Producer
-import json
-import time
+import logging
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# CONFIG SERVER AND SETTINGS
 def config_producer():
-    # CONFIG SERVER AND SETTINGS
-    config = {
-        'bootstrap.servers': 'kafka_host:9093'
-    }
+    try:
+        config = {
+            'bootstrap.servers': 'kafka_host:9093'
+        }
 
-    # CREATE INSTANCE OF PRODUCER
-    producer = Producer(config)
-    return producer
+        # CREATE INSTANCE OF PRODUCER
+        producer = Producer(config)
+        return producer
+    except Exception as e:
+        logger.error(f'Error: failed in the configuration of the kafka broker - {str(e)}')
+        raise e
 
 
 # MANAGE THE DELIVERY MESSAGES
@@ -21,22 +28,34 @@ def delivery_report(err, msg):
         print(f'Mensaje enviado a {msg.topic()} [{msg.partition()}]')
 
 
+# CREATE MESSAGE
 def create_message(signature_user_vote, uuid):
-    # CREATE MESSAGE
-  
-    message_value = signature_user_vote
+    try:
+        if not isinstance(signature_user_vote, bytes) or not isinstance(uuid, str):
+            logger.error(f'Error: The signature must be in bytes and uui in string format')
+            raise TypeError
+        
+        message_value = signature_user_vote
+        key = uuid
+        topic = 'vote_passthrough'
 
-    key = uuid
-    topic = 'vote_passthrough'
-    return topic, key, message_value
+        return topic, key, message_value
+    except Exception as e:
+        logger.error(f'Error: The message could not created - {str(e)}')
+        raise e
 
 
+# INIT THE PRODUCER
 async def send_to_kafka(signature_user_vote, uuid):
-    producer = config_producer()
-    topic, key, message_value = create_message(signature_user_vote, uuid)
+    try:
+        producer = config_producer()
+        topic, key, message_value = create_message(signature_user_vote, uuid)
 
-    # SEND THE MESSAGE
-    producer.produce(topic, key=key, value=message_value, callback=delivery_report)
+        # SEND THE MESSAGE
+        producer.produce(topic, key=key, value=message_value, callback=delivery_report)
 
-    # WAIT UNTIL ALL THE MESSAGES ARRIVES
-    producer.flush() 
+        # WAIT UNTIL ALL THE MESSAGES ARRIVES
+        producer.flush() 
+    except Exception as e:
+        logger.error(f'Error: To send the message to kafka producer - {str(e)}')
+        raise e
