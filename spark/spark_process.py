cb6f17092a60c328d836d5df6dd07cbc45632468
jarodriguezf <jarodriguezf123@gmail.com>
2024-08-23 19:30:34 +0200

    spark process init script created -only read the data from kafka by now-

---
+++ spark/spark_process.py
@@ -0,0 +1,98 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
+import logging
+import time
+
+
+logging.basicConfig(level=logging.DEBUG,
+                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('py4j')
+logger.setLevel(logging.ERROR)
+
+spark_logger = logging.getLogger('org.apache.spark')
+spark_logger.setLevel(logging.ERROR)
+
+
+# SPARK SESSION
+def spark_session():
+    try:
+        spark = SparkSession.builder \
+            .appName("SparkKafkaStreaming") \
+            .getOrCreate()
+        logger.info('Spark context created successfully')
+        return spark
+    except Exception as e:
+        logger.error(f'Error: Spark context can\'t be built - {str(e)}')
+        raise e
+
+
+# READ FROM KAFKA IN STREAM
+def read_from_stream_kafka(spark):
+    try:
+        kafka_stream = spark \
+            .readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "kafka_host:9093") \
+            .option("subscribe", "vote_passthrough") \
+            .option("startingOffsets", "earliest") \
+            .load()
+        logger.info('Data successfully read from kafka stream')
+        return kafka_stream
+    except AnalysisException as e:
+        logger.error(f'Error: In the processing of data from kafka - {str(e)}')
+        raise e
+    except Exception as e:
+        logger.error(f'Error: Unexpected error - {str(e)}')
+        raise e
+
+
+# PAUSE 10 SECOND UNTIL PROCESS THE NEXT DATA
+def process_batch(df, epoch_id):
+    logger.info(f'Processing batch {epoch_id}')
+    try:
+        df.show(truncate=False)
+        time.sleep(10) 
+    except Exception as e:
+        logger.error(f'Error: Failed to process batch - {str(e)}')
+
+
+# CASTING THE DATA IN DF
+def casting_data(kafka_stream):
+    try:
+        df = kafka_stream.selectExpr("CAST(key AS STRING)", "value")
+        logger.info('Casting realized correctly')
+        return df
+    except ValueError as e:
+        logger.error(f'Error: type not found - {str(e)}')
+        raise e
+    except Exception as e:
+        logger.error(f'Error: Unexpected error - {str(e)}')
+        raise e
+
+
+# PRINT THE VALUES IN CONSOLE
+def print_values_from_kafka():
+    spark = spark_session()
+    kafka_stream = read_from_stream_kafka(spark)
+    df = casting_data(kafka_stream)
+
+    try:
+        query = df.writeStream \
+            .foreachBatch(process_batch) \
+            .outputMode("append") \
+            .format("console") \
+            .start()
+
+        logger.info('Streaming query started')
+        logger.info('Awaiting termination...')
+
+        query.awaitTermination()
+    except Exception as e:
+        logger.error(f'Error: Streaming query failed - {str(e)}')
+        raise e
+    finally:
+        logger.info('Spark session closed')
+        spark.stop()
+
+if __name__ == '__main__':
+    print_values_from_kafka()
